8e1d53b16863108f81e45281272301e759235abb
juliana juarez <juulijuarez@gmail.com>
2022-06-11 16:40:36 -0300

    add count-passengers

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,8 @@ class Trip < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
+
+  def passengers_count
+    self.bookings.map { |b| b.passengers if b.status == "Accepted" }.compact.sum
+  end
 end
