f77b45fafe54bf28fa7bb2f3add7a221ef3eb87a
Sergio Sosa Romero <smatias.sosa@gmail.com>
2022-06-11 17:59:47 -0300

    geocoder y parte de mapbox

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,10 @@
 class Trip < ApplicationRecord
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
+  geocoded_by :starting_point
+  after_validation :geocode, if: :will_save_change_to_starting_point?
 
   def passengers_count
     self.bookings.map { |b| b.passengers if b.status == "Accepted" }.compact.sum
