63e5c0cd0bd0daa81bdb9baceb9cccfd9d52c636
alan vergara <alan_vergara_p@hotmail.com>
2022-06-14 20:55:49 -0500

    now can upload profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :trips, through: :bookings
   has_many :bookings
+  has_one_attached :profile_picture
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
