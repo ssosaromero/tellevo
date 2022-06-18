32af3c6328ed9245d611ef75929ddd08be8d65a2
Fabrizio Oyarce <fso1993@gmail.com>
2022-06-18 12:27:24 -0400

    created a beautifull view for my profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def my_profile
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   put "/accept_booking/:id", to: "bookings#accept_booking", as: :accept_booking
   put "/reject_booking/:id", to: "bookings#reject_booking", as: :reject_booking
   get "/my_bookings", to: "bookings#my_bookings"
+  get "/my_profile", to: "pages#my_profile", as: :my_profile
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :trips do
