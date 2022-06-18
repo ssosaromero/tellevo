7abda29bf3717f9ed7ffaa940295c64a67d2105c
alan vergara <alan_vergara_p@hotmail.com>
2022-06-18 14:01:54 -0500

    fixed small issues on the views

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,6 +5,7 @@ class Trip < ApplicationRecord
   has_many :users, through: :bookings
   geocoded_by :starting_point
   after_validation :geocode, if: :will_save_change_to_starting_point?
+  validates :starting_point, :end_point, :number_of_passengers, presence: true
 
   def passengers_count
     self.bookings.map { |b| b.passengers if b.status == "Accepted" }.compact.sum
