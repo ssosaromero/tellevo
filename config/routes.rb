4563a1077e0bd245b20b9627a8cecf38187eabf1
Sergio Sosa Romero <smatias.sosa@gmail.com>
2022-10-15 12:50:28 -0300

    rooteo a home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => :registrations }
 
-  # root to: 'pages#home'
-  root to: 'trips#index'
+  root to: 'pages#home'
+  # root to: 'trips#index'
   get "/my_trips", to: "trips#my_trips"
   put "/accept_booking/:id", to: "bookings#accept_booking", as: :accept_booking
   put "/reject_booking/:id", to: "bookings#reject_booking", as: :reject_booking
