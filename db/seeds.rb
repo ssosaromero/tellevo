5d5616db288c692f22cc454340f2132f6b3c66cb
Sergio Sosa Romero <smatias.sosa@gmail.com>
2022-10-20 20:21:59 -0300

    primera seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Sergio_________________________
+sergio = User.create!(
+  first_name: 'Sergio',
+  last_name: 'Sosa',
+  email: 'sergio@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  )
