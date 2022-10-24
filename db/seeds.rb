3c666491cc7c827790336036a2bf7216f6dd6bac
Sergio Sosa Romero <smatias.sosa@gmail.com>
2022-10-24 18:03:13 -0300

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,35 @@ sergio = User.create!(
   password: '123456',
   password_confirmation: '123456',
   )
+# Alberto_________________________
+alberto = User.create!(
+  first_name: 'Alberto',
+  last_name: 'Garcia',
+  email: 'alberto@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  )
+# Jorge_________________________
+jorge = User.create!(
+  first_name: 'Jorge',
+  last_name: 'Jordan',
+  email: 'jorge@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  )
+# Ruben_________________________
+ruben = User.create!(
+  first_name: 'Ruben',
+  last_name: 'McArthur',
+  email: 'ruben@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  )
+# Claudio_________________________
+sergio = User.create!(
+  first_name: 'Claudio',
+  last_name: 'DeClaudio',
+  email: 'claudio@gmail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  )
